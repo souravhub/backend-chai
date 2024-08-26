04969d64215451a239406eb9bf9dd5cae488c59b
Sourav Samanta <souravthewriter95@gmail.com>
2024-08-26 15:22:32 +0530

    basic controller and routes

--- src/app.js
+++ src/app.js
@@ -14,4 +14,12 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes
+import useRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", useRouter);
+
+// http://localhost:8000/api/v1/users/register
+
 export { app };
